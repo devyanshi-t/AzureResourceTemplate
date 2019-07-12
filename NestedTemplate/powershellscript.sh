496bf4b29dd489cb12b69824d48962ce0e75f8fa
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2019-07-12 13:25:57 +0530

    changes to master

--- NestedTemplate/powershellscript.sh
+++ NestedTemplate/powershellscript.sh
@@ -1,7 +1,7 @@
-Connect-AzAccount
 
-$ResourceGroupName="DevyanshiFirstApp"
-$customport=3307
+# this is a code which creates a new security rule in case of disaster to recover the content of the database subnet.
+$ResourceGroupName="DevyanshiFirstApp"# Add your own resource group accordingly
+$customport=3308# add your own custom port
 
 $rule1="DisasterRecoveryRule1"
 $rule2="DisasterRecoveryRule2"
@@ -14,14 +14,14 @@ $nsg1 = Get-AzNetworkSecurityGroup -Name $nsgname1 -ResourceGroupName $ResourceG
 $nsg2 = Get-AzNetworkSecurityGroup -Name $nsgname2 -ResourceGroupName $ResourceGroupName
 
 # Add the inbound security rule.
-$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rule1 -Description "Allow Inbound Coomunication from databse2 to to database1" -Access Allow `
--Protocol * -Direction Inbound -Priority 3891 -SourceAddressPrefix "20.0.4.0/24" -SourcePortRange $customport `
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rule1 -Description "Allow Inbound Coomunication from databse subnet 2 to to database subnet 1" -Access Allow `
+-Protocol * -Direction Inbound -Priority 3000 -SourceAddressPrefix "20.0.4.0/24" -SourcePortRange $customport `
 -DestinationAddressPrefix "10.0.4.0/24" -DestinationPortRange $customport
 
 
 # Add the outbound security rule.
-$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rule2 -Description "Allow otbound Communication from databse2 to to database1" -Access Allow `
--Protocol * -Direction Outbound -Priority 3891 -SourceAddressPrefix "10.0.4.0/24" -SourcePortRange $customport `
+$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rule2 -Description "Allow otbound Communication from databse subnet 2 to to database subnet 1" -Access Allow `
+-Protocol * -Direction Outbound -Priority 3000 -SourceAddressPrefix "10.0.4.0/24" -SourcePortRange $customport `
 -DestinationAddressPrefix "20.0.4.0/24" -DestinationPortRange $customport
 
 # Update the NSG.
--- ReadMe.md
+++ ReadMe.md
@@ -13,5 +13,4 @@
 
 
 
-## Author
-[`Devyanshi Tiwari`](https://github.com/devyanshi-t)<br />
+
